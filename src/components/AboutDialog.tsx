0f26a659a7a764ef2a8567a9fa3cd3047275ef6e
Earl St Sauver <estsauver@gmail.com>
2024-12-27 11:58:45 +0100

    Update AboutDialog.tsx

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -19,9 +19,12 @@ const AboutDialog = ({ open, onOpenChange }: AboutDialogProps) => {
         <DialogHeader className="space-y-4">
           <DialogTitle className="font-beth-ellen text-2xl">About Me</DialogTitle>
           <DialogDescription className="text-lg">
-            Hi, I'm Earl St Sauver. I'm passionate about building technology that makes a difference.
-            Currently working on exciting projects at Apollo Agriculture, where we're leveraging technology
-            to empower farmers across Africa.
+            Hi, I'm Earl St Sauver, enthusiastic geek and reluctant executive. 
+            
+            I'm a deep believer that humanities greatest leverage lies where others won't tread. I've always liked problems that feel impossible, but am trying to reform myself now that I have children. 
+
+            I started Apollo Agriculture with Eli and Ben to bring modern farming to everyone. I wanted to clear the bottom level of Maslow's hierarchy of needs in our lifetime, for everyone. 
+            What's the point of building aligned AIs if humans are not worth saving?              
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
