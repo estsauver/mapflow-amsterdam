6ae3ae96cb3ba3dc119edaaa251d4eba5797401a
Earl St Sauver <estsauver@gmail.com>
2024-12-30 11:21:05 +0100

    Update AboutDialog.tsx

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -32,10 +32,6 @@ const AboutDialog = ({ open, onOpenChange }: AboutDialogProps) => {
               I started Apollo Agriculture with Eli and Ben to bring modern farming to everyone. 
               I wanted to clear the bottom level of Maslow's hierarchy of needs in our lifetime, for everyone.
             </p>
-
-            <p>
-              Currently just in awe of the acceleration of technology and trying to keep swimming in the flood. 
-            </p>
             
           </DialogDescription>
         </DialogHeader>
